fbf73bc56203590b7e1b6f25df26cfe8e46e5080
Marcin <marcin_wlodarczyk@protonmail.com>
2022-01-16 11:03:21 +0100

    finished chapter 11: introduced concept of hitboxes and hurtboxes, implemented them. Setting collision only with animation and set them to the side of attack. Collision layers.

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,9 +1,12 @@
 extends Node2D
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		grassEffect.global_position = global_position
-		world.add_child(grassEffect)
-		queue_free()
+
+func play_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	grassEffect.global_position = global_position
+	world.add_child(grassEffect)
+	
+func _on_Hurtbox_area_entered(area):
+	play_effect()
+	queue_free()
