fad4490b03ea0679e95ace24058fd2ce0e8721e1
Marcin <marcin_wlodarczyk@protonmail.com>
2022-01-16 11:32:53 +0100

    finished chapter 12: added roll state with its animations and transitions

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 const ACCELERATION = 500
 const MAX_SPEED = 80
+const ROLL_SPEED = 125
 const FRICTION = 500
 
 enum {
@@ -12,6 +13,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
@@ -20,12 +22,12 @@ onready var animation_state = animation_tree.get("parameters/playback")
 func _ready():
 	animation_tree.active = true
 	
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)	
 
@@ -35,12 +37,14 @@ func move_state(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		# moving
 		
 		#animation stuff
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
 		animation_tree.set("parameters/Attack/blend_position", input_vector)
+		animation_tree.set("parameters/Roll/blend_position", input_vector)
 		
 		animation_state.travel("Run")
 		#moving stuff
@@ -56,14 +60,28 @@ func move_state(delta):
 		#moving stuff
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	#move_and_collide(velocity * delta)
-	velocity = move_and_slide(velocity)
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-		
+	elif Input.is_action_just_pressed("roll"):
+		state = ROLL	
+	
 func attack_state(delta):
 	velocity = Vector2.ZERO
 	animation_state.travel("Attack")
 
 func attack_animation_finished():
 	state = MOVE
+
+func roll_animation_finished():
+	velocity = velocity * 0.3
+	state = MOVE
+	
+func roll_state(delta):
+	velocity = ROLL_SPEED * roll_vector
+	animation_state.travel("Roll")
+	move()
+	
+func move():
+	velocity = move_and_slide(velocity)
