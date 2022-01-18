211ddd806c4c6256252eacc2b38761a07b1f5be7
Marcin <marcin_wlodarczyk@protonmail.com>
2022-01-18 21:50:40 +0100

    finished chapter 14: added node with stats for enemy, use export vars and setget

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,10 +2,19 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 100;
+	stats.health -= area.damage
+	if stats.health <= 0:
+		queue_free()
 	#queue_free()
+
+
+func _on_Stats_no_health():
+	queue_free()
