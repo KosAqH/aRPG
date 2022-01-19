d36dec9a94ce7e2283b77213fafe51de7b9763c1
Marcin <marcin_wlodarczyk@protonmail.com>
2022-01-19 22:22:19 +0100

    started chapter 16: added hit effect and began work on bat AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,15 +1,39 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
 
 onready var stats = $Stats
 
 const DeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
-	knockback = move_and_slide(knockback)
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
 
+var state = IDLE
+
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, 200 * FRICTION)
+	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity.move_toward(Vector2.ZERO, 200 * FRICTION)
+		WANDER:
+			pass
+		CHASE:
+			pass
+
+func seek_player():
+	pass
+	
 func _on_Hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 100;
 	stats.health -= area.damage
