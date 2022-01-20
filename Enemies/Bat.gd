48ae8e0702002af04c4d774aceced9808faae4f1
Marcin <marcin_wlodarczyk@protonmail.com>
2022-01-20 12:47:43 +0100

    finished chapter 16: bat can follow the player, his sprite is flipped when he changes direction in x-axis

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -7,7 +7,9 @@ export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
 
+onready var sprite = $BatSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 const DeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
@@ -25,14 +27,23 @@ func _physics_process(delta):
 	
 	match state:
 		IDLE:
-			velocity.move_toward(Vector2.ZERO, 200 * FRICTION)
+			velocity = velocity.move_toward(Vector2.ZERO, 200 * FRICTION)
+			seek_player()
 		WANDER:
 			pass
 		CHASE:
-			pass
-
+			var player = playerDetectionZone.player
+			print(player)
+			if player != null:
+				var direction = player.global_position - global_position
+				velocity = velocity.move_toward(direction.normalized() * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+	sprite.flip_h = velocity.x < 0 #obraca nietoperza w prawo-lewo
+	velocity = move_and_slide(velocity)
 func seek_player():
-	pass
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 	
 func _on_Hurtbox_area_entered(area):
 	knockback = area.knockback_vector * 100;
